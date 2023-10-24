20c8c09657b45b69234c2649e7b745a4e8316ed2
lucasmacpherson <122807982+lucasmacpherson@users.noreply.github.com>
2023-10-24 01:13:46 +0100

    Refactored raytracing methods

---
+++ raytracer.py
@@ -0,0 +1,77 @@
+import numpy as np
+
+def vector_to_receiver(hstat, recevier):
+    """
+    Calculate the normalised direction vector pointing torwards receiver
+    """
+    return np.array(norm_vector(recevier - hstat))
+
+def calculate_mirror_normal(receiver_vec, incident_vec):
+    """
+    Calculate the mirror normal vector required for reflection of incident rays to the target
+    """
+    xproduct = np.cross(-incident_vec, receiver_vec)
+    theta = np.arcsin(magnitude(xproduct))
+    if magnitude(xproduct != 0):
+        R = calculate_rotation_matrix(xproduct/magnitude(xproduct), theta/2)
+    else:
+        R = calculate_rotation_matrix(xproduct, theta/2)
+        
+    if theta != 0:
+        return np.array(np.matmul(R, -incident_vec))
+    else:
+        return -incident_vec
+
+def calculate_reflection(mirror_norm, incident_vec):
+    """
+    Calculate the normalised direction vector of the reflected ray
+    """
+    return norm_vector(np.array(incident_vec - 2*mirror_norm.dot(incident_vec.dot(mirror_norm))))
+
+def calculate_mirror_positions(hstat, mirror_normal, receiver_vec, reflecting_width):
+    step = reflecting_width/4
+    xproduct = norm_vector(np.cross(mirror_normal, receiver_vec))
+    return np.array([hstat - step*xproduct, hstat + step*xproduct])
+
+def calculate_target_intersection(reflected_vec, hstat, receiver_pos):
+    plane_normal = np.array([0, 0, 1])
+    plane_offset = receiver_pos[2]
+
+    return calculate_plane_intersection(reflected_vec, hstat, plane_normal, plane_offset)
+
+# Generalised vector methods
+
+def magnitude(vector: np.ndarray):
+    """ 
+    Faster method for getting the magnitude of numpy vectors
+    """
+    return np.sqrt(vector.dot(vector))
+
+def norm_vector(vector: np.ndarray):
+    """
+    Get the normalised vector of the direction of any given numpy vector
+    """
+    return vector / np.sqrt(vector.dot(vector))
+
+def calculate_rotation_matrix(u: np.ndarray, theta):
+    # Source: https://stackoverflow.com/questions/17763655/rotation-of-a-point-in-3d-about-an-arbitrary-axis-using-python
+    stheta = np.sin(theta)
+    ctheta = np.cos(theta)
+
+    M = [ [ctheta+u[0]*u[0]*(1-ctheta), u[0]*u[1]*(1-ctheta) - u[2]*stheta, u[0]*u[2]*(1-ctheta) + u[1]*stheta],
+          [u[1]*u[0]*(1-ctheta) + u[2]*stheta, ctheta + u[1]*u[1]*(1-ctheta), u[1]*u[2]*(1-ctheta) - u[0]*stheta],
+          [u[2]*u[0]*(1-ctheta) - u[1]*stheta, u[2]*u[1]*(1-ctheta) + u[0]*stheta, ctheta + u[2]*u[2]*(1-ctheta)]
+    ]
+
+    return M
+    
+def calculate_plane_intersection(vector_norm: np.ndarray, vector_position: np.ndarray, plane_norm: np.ndarray, plane_offset):
+    # Source: https://stackoverflow.com/questions/26920705/ray-plane-intersection
+    s = plane_norm.dot(vector_norm)
+    if s == 0: 
+        return None
+    
+    t = (plane_offset-(plane_norm.dot(vector_position)))/s
+    return np.array([vector_position[0] + vector_norm[0]*t, 
+                     vector_position[1] + vector_norm[1]*t,
+                     vector_position[2] + vector_norm[2]*t])
--- testing.py
+++ testing.py
@@ -1,38 +1,26 @@
 from matplotlib import pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 
+from raytracer import *
 from heliostat_raytracer import *
 
-hstats = create_heliostat_field(20, [5, 5])
+hstats = create_heliostat_field(2, [2, 2])
+receiver_pos = np.array([0, 0, 1])
 incident_vec = norm_vector(np.array([0.5, 0, -1]))
 
-receiver_pos = np.array([0, 0, 10])
-receiver_vecs = get_directions_to_receiver(hstats, receiver_pos)
-mirror_norms = calculate_mirror_normals(receiver_vecs, incident_vec)
-reflected_vecs = calculate_reflected_rays(mirror_norms, incident_vec)
-
-mirror_pos = calculate_mirror_positions(hstats, mirror_norms, receiver_vecs, reflecting_width=0.1)
-reflected_vecs_m = reflected_vecs.repeat(2, axis=0)
+model = raytrace_heliostat_field(hstats, incident_vec, receiver_pos, reflecting_width=0.1)
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-ax.quiver(mirror_pos[:, 0], mirror_pos[:, 1], mirror_pos[:, 2], 
-            reflected_vecs_m[:, 0],reflected_vecs_m[:, 1], reflected_vecs_m[:, 2], 
+ax.quiver(model['mirror_positions'][:, 0], model['mirror_positions'][:, 1], model['mirror_positions'][:, 2], 
+            model['reflected_vectors'][:, 0],model['reflected_vectors'][:, 1], model['reflected_vectors'][:, 2], 
             color='b', label="Reflected Rays")
-ax.quiver(hstats[:, 0], hstats[:, 1], hstats[:, 2], 
-            mirror_norms[:, 0],mirror_norms[:, 1], mirror_norms[:, 2], 
+ax.quiver(model['mirror_positions'][:, 0], model['mirror_positions'][:, 1], model['mirror_positions'][:, 2], 
+            model['mirror_normals'][:, 0],model['mirror_normals'][:, 1], model['mirror_normals'][:, 2], 
             color='orange', label="Mirror Normals")
 ax.quiver(hstats[:, 0], hstats[:, 1], hstats[:, 2], 
             -incident_vec[0], -incident_vec[1], -incident_vec[2], 
             color='red', label="Incident Vector")           
 ax.scatter(receiver_pos[0], receiver_pos[1], zs=receiver_pos[2])
 ax.legend()
-plt.show()
-
-intersections = calculate_target_intersections(reflected_vecs_m, mirror_pos, receiver_pos)
-
-fig = plt.figure()
-ax = fig.add_subplot()
-ax.scatter(intersections[:, 0], intersections[:, 1], marker='o')
 plt.show()
