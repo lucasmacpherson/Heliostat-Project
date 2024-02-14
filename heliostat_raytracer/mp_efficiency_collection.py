Fix collection fraction calculation

heliostat_raytracer/mp_efficiency_collection.py
@@ -28,14 +28,14 @@ if __name__ == "__main__":
     hstats = [[0.226, -0.226, 0], [0.226, 0.226, 0]]
     tilts = np.array([tilt_deg * np.pi/180]).repeat(2*len(hstats))
 
-    raycasts = 100**2
+    raycasts = 4000**2
     beam_size = 2.5
     start_height = 0.25
 
     # Incident ray only in -x direction (-x, 0, -1)
     # elevations = np.arange(10, 80, 5)
-    elevations = np.array((35, 45))
-    azimuths = np.arange(-70, 70, 30)
+    elevations = np.array((45, 65))
+    azimuths = np.arange(-80, 80, 10)
 
     for i, elevation in enumerate(elevations):
         args = []
@@ -44,14 +44,15 @@ if __name__ == "__main__":
             incident_vec = -1*vector_from_elevation_azimuth(azimuth, elevation)
             args.append([hstats, incident_vec, receiver_pos, heliostat_width, receiver_size, 
                         mirror_size, beam_size, raycasts, start_height, tilts, (-1, 2),
-                        f"data/images/{elevation}_{azimuth}_4Mrays_points.png"])
+                        f"data/images/{elevation}_{azimuth}_16Mrays_points.png"])
 
-        with mp.Pool(worker_threads) as pool:
+        # with mp.Pool(worker_threads) as pool:
+        with mp.Pool() as pool:
             efficiency_results = pool.starmap(mphelper_efficiency_imagegen, args)
 
         efficiencies = []
         for result in efficiency_results:
             efficiencies.append(result)
 
-        np.savetxt(f'data/{elevation}_4M_efficiencies.csv', np.column_stack((azimuths, np.array(efficiencies))), delimiter=',')
+        np.savetxt(f'data/{elevation}_16M_efficiencies.csv', np.column_stack((azimuths, np.array(efficiencies))), delimiter=',')
 