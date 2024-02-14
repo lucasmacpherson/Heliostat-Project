Fix overflow error in background removal

Image analysis v3/data_writer_v2.py
@@ -1,8 +1,9 @@
 from edge_detector_v3 import *
 
 # data set 1 variables
-# degrees = [15,25,35,45,55,65]
-# location = "Images 15_12/"
+# degrees = [5,15,25,35,45,55,65]
+# degrees = [45]
+# location = "Image analysis v3/Images 15_12/"
 # image_labels = [1,2,3,4,5,6]
 # date = "15_12"
 # xy_range = [[60, 1024], [0, 1280]] # in order y, x
@@ -11,7 +12,7 @@ from edge_detector_v3 import *
 
 # data set 2 variables
 degrees = [5,15,25,35,45,55]
-location = "Images 23_1/"
+location = "Image analysis v3/Images 23_1/"
 image_labels = [1,2,3,4,5]
 date = "23_1"
 xy_range = [[90, 1024], [0, 1280]] # in order y, x
@@ -41,10 +42,12 @@ for deg in degrees:
         imgtitle = date + " data/figs/" + str(deg) + " " + str(num) + ".png"
         
         image, background = trim(img, back, xy_range[0], xy_range[1])
+        image = np.array(image, dtype=np.int16)
+        background = np.array(background, dtype=np.int16)
 
         removed = np.array(image) - np.array(background)
 
-        binary = gen_binary(removed, title, imgtitle, threshold=10)
+        binary = gen_binary(removed, imgtitle, threshold=10)
 
         data = obj_data_extractor(binary, image)
 
@@ -55,9 +58,9 @@ for deg in degrees:
 
         header = "areas, intensities, x, y"
 
-        #np.savetxt(title, data, header = header, delimiter = ",")
+        np.savetxt(title, data, header = header, delimiter = ",")
 
     np.savetxt((title[:-6] + " distances.csv"), distances, delimiter = ",")
     np.savetxt((title[:-6] + " w distances.csv"), w_distances, delimiter = ",")
 
-
+ 

Image analysis v3/display_v2.py
@@ -19,10 +19,11 @@ def plot_area_by_degree(header, degrees, iterations, avg_per_image = False, avg_
                 col = colours[num-1]
 
                 areas = data.T[0]
+                print(areas)
                 
                 if not avg_per_image:
                     for a in areas:
-                            plt.scatter(deg, a, color = col, label = num)
+                             plt.scatter(deg, a, color = col, label = num)
                 
                 elif avg_per_image:
                     print(areas)
@@ -375,35 +376,30 @@ def weighted_pixel_dist(header, degrees, iterations, target_loc):
 #data set 1 variables
 degrees = [5,15,25,35,45,55,65]
 iterations = 6
-header = "15_12 data/locs/"
+header = "Image analysis v3/15_12 data/locs/"
 target_loc = [644, 566]  
 
 #data set 2 variables
 # degrees = [5,15,25,35,45,55]
 # iterations = 5
-# header = "23_1 data/locs/"
+# header = "Image analysis v3/23_1 data/locs/"
 # target_loc = [616, 571]
 
 colours = ["red", "orange", "yellow", "green", "blue", "purple"]
+#x = np.array([57.5, 52.5, 47.5, 42.5, 37.5, 32.5, 27.5])
+x = np.array([64.5, 59.5, 54.5, 49.5, 44.5, 39.5, 34.5])
+y = np.cos(x*(np.pi/180))
 
-
-
-plot_area_by_degree(header, degrees, iterations, avg_per_image=True, avg_per_degree=False)
-
-x = np.array([57.5, 52.5, 47.5, 42.5, 37.5, 32.5, 27.5])
-# xsmooth = np.linspace(x[0]*(np.pi/180), x[-1]*(np.pi/180), 60)
-# ysmooth = np.cos(xsmooth) * 16000
-y = np.cos(x*(np.pi/180)) * 15000
-plt.plot(degrees, y) 
-#plt.savefig("23_1 data/Area by image 23_1.png")
-plt.show()
-
-# plot_intensity_by_degree(header, degrees, iterations, avg_per_image=True, avg_per_degree=False)
-# x = np.linspace(np.min(degrees), np.max(degrees), 50)
-# y = 4500 * np.cos((90 - np.array(x)) * (np.pi/180))
-# #plt.plot(x, y)
-# #plt.savefig("23_1 data/Total intensity by image 23_1 w fit.png")
+# plot_area_by_degree(header, degrees, iterations, avg_per_image=True, avg_per_degree=False)
+# plt.plot(degrees, y*18000)  
+# #plt.savefig("23_1 data/Area by image 23_1.png")
+# #plt.savefig("15_12 data/Area by image 15_12.png")
 # plt.show()
+
+plot_intensity_by_degree(header, degrees, iterations, avg_per_image=True, avg_per_degree=False)
+plt.plot(degrees, y*240000)
+#plt.savefig("23_1 data/Total intensity by image 23_1 w fit.png")
+plt.show()
 
 # plot_average_intensity(header, degrees, iterations, avg_per_image=True, avg_per_degree=False)
 # #plt.ylim(0,2)

Image analysis v3/edge_detector_v3.py
@@ -25,7 +25,7 @@ def trim(img, background, y_range, x_range, back_given = True):
     
     return image, b_image
 
-def gen_binary(image, title: str, imgtitle, savefig = True, savedata = True, threshold = 10): 
+def gen_binary(image, imgtitle, savefig = True, threshold = 10): 
     """generates and saves binary version of image, 1 for object 0 for background"""
 
     dim = np.shape(image)
@@ -44,14 +44,11 @@ def gen_binary(image, title: str, imgtitle, savefig = True, savedata = True, thr
 
     if savefig: 
         plt.figure()
-        plt.title(title)
+        plt.title(imgtitle[-8:-4])
         plt.imshow(closed)
         plt.savefig(imgtitle)
         #plt.show()
         plt.close()
-
-    if savedata:
-        np.savetxt(title, closed, fmt='%i')
 
     return closed
 