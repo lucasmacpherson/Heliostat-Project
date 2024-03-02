Add fixed normalisation to images

heliostat_raytracer/images.py
@@ -58,13 +58,13 @@ def create_intensity_distribution(points, image_size, sigma):
     # Normalize the image
     max_intensity = np.max(image)
     if max_intensity > 0:
-        image = (image / max_intensity * 255).astype(np.uint8)
+        image = (image / 8 * 255).astype(np.uint8)
 
     # Create and return the PIL image
     return Image.fromarray(image, 'L')
 
 def intensity_image(model, image_size, sigma):
-    rays = model['rays']
+    rays = get_rays_at_target(model)
     receiver_pos = model['receiver_position']
 
     points = []

heliostat_raytracer/mp_efficiency_collection.py
@@ -21,7 +21,7 @@ ylim = (-1, 2)
 hstat_layout = [2, 2]
 tilt_deg = -10
 
-worker_threads = 16
+worker_threads = 12
 
 if __name__ == "__main__":
     mp.freeze_support()
@@ -30,7 +30,7 @@ if __name__ == "__main__":
     tilts = np.array([tilt_deg * np.pi/180]).repeat(2*len(hstats))
 
     raycasts = 5000**2
-    beam_size = 2.5
+    beam_size = 1.6
     start_height = 0.25
 
     # Incident ray only in -x direction (-x, 0, -1)