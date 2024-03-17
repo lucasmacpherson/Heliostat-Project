Fix theta calculation - replace xprod with dprod

heliostat_raytracer/heliostat.py
@@ -12,8 +12,9 @@ def calculate_mirror_normal(receiver_vec, incident_vec):
     """
     Calculate the mirror normal vector required for reflection of incident rays to the target
     """
+    dproduct = np.dot(-incident_vec, receiver_vec)
     xproduct = np.cross(-incident_vec, receiver_vec)
-    theta = np.arcsin(magnitude(xproduct))
+    theta = np.arccos(dproduct)
     if magnitude(xproduct != 0):
         R = calculate_rotation_matrix(xproduct/magnitude(xproduct), theta/2)
     else:

heliostat_raytracer/mp_efficiency_collection.py
@@ -13,7 +13,11 @@ heliostat_width = exp.HELIOSTAT_WIDTH.value # Mirrors are seperated by 28.5cm
 mirror_size = exp.MIRROR_RADIUS.value
 receiver_pos = exp.RECEIVER_POSITION.value
 receiver_size = exp.RECEIVER_SIZE.value
-ylim = exp.YLIM.value
+source_dist = exp.SOURCE_DISTANCE.value
+system_extent = np.array([
+    np.array((0.3, 0.5, -0.3)),
+    np.array((-0.2, -0.5, 0.2))
+])
 # Receiver is located 8.7cm back, at a height of 58.5cm. Heliostat receivers are
 # located 20.3cm 
 
@@ -21,7 +25,7 @@ ylim = exp.YLIM.value
 # hstat_layout = [2, 2]
 tilt_deg = -10
 
-worker_threads = 4
+worker_threads = 8
 
 if __name__ == "__main__":
     mp.freeze_support()
@@ -29,9 +33,7 @@ if __name__ == "__main__":
     hstats = [[0.226, -0.226, 0], [0.226, 0.226, 0]]
     tilts = np.array([tilt_deg * np.pi/180]).repeat(2*len(hstats))
 
-    raycasts = 5000**2
-    beam_size = 3.0
-    start_height = 0.20
+    raycasts = (1000, 25000)
 
     # Incident ray only in -x direction (-x, 0, -1)
     elevations = np.array((15, 30, 45, 55, 60))
@@ -44,7 +46,7 @@ if __name__ == "__main__":
 
         for i, azimuth in enumerate(azimuths):
             args.append([hstats, elevation, azimuth, receiver_pos, heliostat_width, receiver_size, 
-                        mirror_size, beam_size, raycasts, start_height, tilts, (-1, 2),
+                        mirror_size, source_dist, system_extent, raycasts, tilts, (-1, 2),
                         f"data/images/{elevation}_{azimuth}_25Mrays_intensity.png"])
 
         with mp.Pool(worker_threads) as pool:
@@ -55,5 +57,5 @@ if __name__ == "__main__":
             # pool.starmap() preserves order of arguments in results returned
             efficiencies[(elevation, azimuths[i])] = result
 
-    with open('data/16Mrays_last.pkl', 'wb') as file:
+    with open('data/25Mrays_last.pkl', 'wb') as file:
         pkl.dump(efficiencies, file)

heliostat_raytracer/test_scripts/low_elev_analysis.py
@@ -18,19 +18,18 @@ system_extent = np.array([
     np.array((-0.2, -0.5, 0.2))
 ])
 
-azimuth = -70
-elevation = 60
+azimuth = 0
+elevation = 30
 incident_vec = -1*vector_from_azimuth_elevation(azimuth, elevation)
-apparent_inc_vec = norm_vector(incident_vec + np.array((0, 0, 0)))
 
 tilt_deg = -10
 tilts = np.array([tilt_deg * np.pi/180]).repeat(2*len(hstats))
 
 # Create aligned model for apparent incident vector
-model = align_heliostat_field(hstats, apparent_inc_vec, exp.RECEIVER_POSITION.value, exp.MIRROR_SEPERATION.value, tilts=tilts)
+model = align_heliostat_field(hstats, incident_vec, exp.RECEIVER_POSITION.value, exp.MIRROR_SEPERATION.value, tilts=tilts)
 
 print(f"Central incident vector: {incident_vec.round(3)} (alpha: {azimuth}, beta: {elevation})")
-print(f"Apparent incident vector: {apparent_inc_vec.round(3)}")
+print(f"Apparent incident vector: {incident_vec.round(3)}")
 receiver_pos = model['receiver_position']
 for i, hstat in enumerate(hstats):
     print(f"""Heliostat {i}: (Position: {hstat.round(3)})