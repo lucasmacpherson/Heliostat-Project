43361ec0242918edb0beaac3bf5491bedb286a2a
lucasmacpherson <122807982+lucasmacpherson@users.noreply.github.com>
2024-04-27 22:45:18 +0100

    Increase range and improve multiprocessing

--- heliostat_raytracer/mp_efficiency_collection.py
+++ heliostat_raytracer/mp_efficiency_collection.py
@@ -26,8 +26,9 @@ system_extent = np.array([
     np.array((-0.2, -0.5, 0.2))
 ])
 source_dist = exp.SOURCE_DISTANCE.value
-theta_range = 8 * np.pi/180
-phi_range = 15 * np.pi/180
+# 8, 15 for experimental range
+theta_range = 45 * np.pi/180
+phi_range = 45 * np.pi/180
 
 # Uniform incidence parameters
 beam_size = 2.0
@@ -35,8 +36,8 @@ start_height = 0.2
 
 # Process parameters
 raycasts = 25_000_000
-worker_threads = 8
-run_name = "exprange_10degtilt_25Mrays"
+worker_threads = 6
+run_name = "4hst_maxrange_idealtilt_25Mrays"
 
 if __name__ == "__main__":
     mp.freeze_support()
@@ -45,10 +46,11 @@ if __name__ == "__main__":
     # tilts = np.array([tilt_deg * np.pi/180]).repeat(2*len(hstats))
     tilts = 'ideal'
 
-    # elevations = np.array((15, 30, 45, 55, 60))
-    elevations = np.array((15, 30, 45, 60))
+    # elevations = np.array((15, 30, 45, 60))
+    azimuths = np.arange(0, 190, 10)
+    elevations = np.arange(0, 90, 5)
     # azimuths = np.array((-70, -60, -45, -30, -15, 0, 15, 30, 45, 60, 70))
-    azimuths = np.array((0, 15, 30, 45, 60, 70))
+    # azimuths = np.array((0, 15, 30, 45, 60, 70))
 
     data = {
         'collection_fractions': {},
@@ -56,23 +58,27 @@ if __name__ == "__main__":
         'heliostat_thetas': {},
         'mirror_thetas': {}
     }
-    for i, elevation in enumerate(elevations):
-        args = []
 
-        for i, azimuth in enumerate(azimuths):
+    args = []
+    for i, elevation in enumerate(elevations):
+        for j, azimuth in enumerate(azimuths):
             args.append([hstats, elevation, azimuth, receiver_pos, mirror_sep, receiver_size, 
                         mirror_size, source_dist, system_extent, raycasts, (theta_range, phi_range), 
                         tilts, (-1, 2), f"data/images/{run_name}/{elevation}_{azimuth}_intensity.png"])
 
-        with mp.Pool(worker_threads) as pool:
-            efficiency_results = pool.starmap(mphelper_alldata_imagegen, args)
+    with mp.Pool(worker_threads) as pool:
+        result_lst = pool.starmap(mphelper_alldata_imagegen, args)
 
-        for i, result in enumerate(efficiency_results):
-            idx = (elevation, azimuths[i])
-            data['collection_fractions'][idx] = result['collection_fraction']
-            data['ideal_mirror_tilts'][idx] = result['ideal_mirror_tilts']
-            data['heliostat_thetas'][idx] = result['heliostat_thetas']
-            data['mirror_thetas'][idx] = result['mirror_thetas']
+    for i, elevation in enumerate(elevations):
+        for j, azimuth in enumerate(azimuths):
+            idx = (i*len(elevations) + j)
+            result = result_lst[idx]
+
+            key = (elevation, azimuth)
+            data['collection_fractions'][key] = result['collection_fraction']
+            data['ideal_mirror_tilts'][key] = result['ideal_mirror_tilts']
+            data['heliostat_thetas'][key] = result['heliostat_thetas']
+            data['mirror_thetas'][key] = result['mirror_thetas']
 
     with open(f'data/{run_name}_last.pkl', 'wb') as file:
         pkl.dump(data, file)
